615fd8708e918ccafed2af50462de0c9f2b046c4
sdiupetter <76866782+sdiupetter@users.noreply.github.com>
2022-06-15 01:21:34 +0800

    Add files via upload

---
+++ extra.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## 添加你需要重启自动执行的任意命令，比如 ql repo
+## 安装node依赖使用 pnpm install -g xxx xxx
+## 安装python依赖使用 pip3 install xxx
+# 安装 dotnet 环境
+echo -e "\n-------set up dot net env-------"
+apk add bash icu-libs krb5-libs libgcc libintl libssl1.1 libstdc++ zlib
+wget https://download.visualstudio.microsoft.com/download/pr/bd94779d-c7c4-47fd-b80a-0088caa0afc6/40f115bbf4c068359e7a066fe0b03dbc/dotnet-sdk-6.0.101-linux-musl-x64.tar.gz
+DOTNET_FILE=dotnet-sdk-6.0.101-linux-musl-x64.tar.gz
+export DOTNET_ROOT=/home/dotnet
+mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
+export PATH=$PATH:$DOTNET_ROOT
+ln -s /home/dotnet/dotnet /usr/local/bin
+dotnet --version
+echo -e "\n-------set up dot net env finish-------"
+
